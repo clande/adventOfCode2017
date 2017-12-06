2fdf0f9b3daf05728654513ab6be645bc6ddfebf
Chris Lande <clande@marchex.com>
2017-12-05 22:02:37 -0800

    1

--- src/main/scala/aoc2017/Day1.scala
+++ src/main/scala/aoc2017/Day1.scala
@@ -1,5 +1,7 @@
 package aoc2017
 
+import scala.util.control.TailCalls.TailRec
+
 class Day1 {
   def getFileAndDoWork = {
     val x = scala.io.Source.fromFile("input/Day1/input").mkString
@@ -8,9 +10,34 @@ class Day1 {
   }
 
   def work(x: String): Int = {
-//    x map(println(_))
+    getTotal(x.toList, 0, x.toList)
+  }
 
+  def determineValue(str: List[Char], originalString: List[Char]): Int = {
+    val nextVal = str.tail match {
+      case Nil => originalString.head.toString.toInt
+      case head :: tail => head.toString.toInt
+    }
 
-    0
+    val prevVal = str.head.toString.toInt
+    if (prevVal == nextVal) {
+      println(s"NON-0 prev: $prevVal, next: $nextVal")
+      nextVal
+    } else {
+      println(s"0 prev: $prevVal, next: $nextVal")
+      0
+    }
+  }
+
+  def getTotal(str: List[Char], acc: Int, originalString: List[Char]): Int = {
+    str match{
+      case Nil =>
+        println(s"NIL - str: $str, acc: $acc")
+        acc
+      case _ =>
+        val value = determineValue(str, originalString)
+        println(s"NOTNIL - str: $str, acc: $acc, value: $value")
+        getTotal(str.tail, acc + value, originalString)
+    }
   }
 }
