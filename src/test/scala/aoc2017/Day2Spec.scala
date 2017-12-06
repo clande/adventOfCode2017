99531313bd10010c15ac6d159a7eecc556aa9e47
Chris Lande <clande@marchex.com>
2017-12-06 09:32:06 -0800

    Day2 Initial

---
+++ src/test/scala/aoc2017/Day2Spec.scala
@@ -0,0 +1,22 @@
+package aoc2017
+
+import org.scalatest.{FunSpecLike, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mockito.MockitoSugar
+
+/**
+  * Created by clande on 12/6/17.
+  */
+class Day2Spec extends FunSpecLike
+  with MockitoSugar
+  with Matchers
+  with ScalaFutures {
+  describe("doWork") {
+    it("should get 18") {
+      val s =
+        """
+          |
+        """.stripMargin
+    }
+  }
+}
