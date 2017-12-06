afc5d4d5e3387398e0778469aa36d0cc4815b3de
Chris Lande <clande@marchex.com>
2017-12-06 08:38:16 -0800

    Day1 Done

--- src/main/scala/aoc2017/Day1.scala
+++ src/main/scala/aoc2017/Day1.scala
@@ -5,39 +5,33 @@ import scala.util.control.TailCalls.TailRec
 class Day1 {
   def getFileAndDoWork = {
     val x = scala.io.Source.fromFile("input/Day1/input").mkString
-//    println(work(x))
-    work(x)
+    println(work(x))
   }
 
   def work(x: String): Int = {
-    getTotal(x.toList, 0, -1,  x.toList.head.toString.toInt, "")
+    getTotal(x.toList, 0, x.toList.head.toString.toInt, "")
   }
 
-  def determineValue(prevVal: Int, str: List[Char], firstVal: Int): Int = {
-    val (nextVal, isLastChar) = str.tail match {
-      case Nil => (firstVal, true)
-      case head :: tail => (head.toString.toInt, false)
+  def determineValue(str: List[Char], firstVal: Int): Int = {
+    val next = str.tail match {
+      case Nil => firstVal
+      case head :: tail => head.toString.toInt
     }
-    val currentVal = str.head.toString.toInt
+    val curr = str.head.toString.toInt
 
-    println(s"prev: $prevVal, curr: $currentVal, next: $nextVal, isLasr: $isLastChar")
-    (prevVal, currentVal, nextVal, isLastChar) match {
-      case (p, c, n, true) if((p == c) && (c == n)) => c + c
-      case (_, c, n, true) if(c == n) => c
-      case (p, c, _, _) if(p == c) => c
-      case (_,_,_,_) => 0
-    }
+    println(s"curr: $curr, next: $next")
+    if (curr == next) curr else 0
   }
 
-  def getTotal(str: List[Char], acc: Int, prev: Int, firstVal: Int, track: String): Int = {
+  def getTotal(str: List[Char], acc: Int, firstVal: Int, track: String): Int = {
     str match{
       case Nil =>
         println(track)
         acc
       case head :: tail =>
-        val value = determineValue(prev, str, firstVal)
+        val value = determineValue(str, firstVal)
         println(s"val: $value, acc: $acc")
-        getTotal(tail, acc + value, head.toString.toInt, firstVal, s"$track$value")
+        getTotal(tail, acc + value, firstVal, s"$track$value")
     }
   }
 }
