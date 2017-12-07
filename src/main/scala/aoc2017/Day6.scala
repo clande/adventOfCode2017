ff20e1442f785260059629589093676cb3b93dbf
Chris Lande <clande@marchex.com>
2017-12-06 19:49:07 -0800

    Day6 initial

--- src/main/scala/aoc2017/Day6.scala
+++ src/main/scala/aoc2017/Day6.scala
@@ -14,12 +14,6 @@ class Day6 {
     currAllocation(largestBankIdx) = 0
     var currBankIdx = incrementBankIndex(largestBankIdx, currAllocation.size - 1)
     1 to (largestBankVal) foreach { _ =>
-//      (currAllocation(currBankIdx), largestBankIdx) match {
-//        case (currVal, lbi) if currVal == largestBankVal && lbi == -1 =>
-//          currAllocation(currBankIdx) = 0
-//          largestBankIdx = currBankIdx
-//        case (a, _) => currAllocation(currBankIdx) = a + 1
-//      }
       currAllocation(currBankIdx) += 1
       currBankIdx = incrementBankIndex(currBankIdx, currAllocation.size - 1)
     }
@@ -29,22 +23,22 @@ class Day6 {
   }
 
   def incrementBankIndex(currBankIdx: Int, maxSize: Int): Int = currBankIdx match {
-    case cbi if(cbi == maxSize) => 0
+    case cbi if cbi == maxSize => 0
     case _ => currBankIdx + 1
   }
 
-  def doWork(banksString: String): Int = {
+  def doWork(banksString: String): (Int, Int) = {
     var banks = banksString.split("\\s+").toList
-
     var dupeDetected = false
     var reallocateCount = 0
     var allocationHistory = List(banks.mkString(" "))
+
     while(!dupeDetected){
       val (newAllocationHistory, newDupeDetected) = reallocateAndCheckForInfiniteLoop(allocationHistory)
       allocationHistory = newAllocationHistory
       dupeDetected = newDupeDetected
       reallocateCount += 1
     }
-    reallocateCount
+    (reallocateCount, allocationHistory.lastIndexOf(allocationHistory.head))
   }
 }
--- src/test/scala/aoc2017/Day6Spec.scala
+++ src/test/scala/aoc2017/Day6Spec.scala
@@ -11,12 +11,12 @@ class Day6Spec extends FunSpecLike
   describe("doWork") {
     val x = new Day6
 
-//    it("should process the file") {
-//      x.getFileAndDoWork
-//    }
+    it("should process the file") {
+      x.getFileAndDoWork
+    }
 
     it("should get 5") {
-      x.doWork("0 2 7 0") shouldBe 5
+      x.doWork("0 2 7 0") shouldBe (5, 4)
     }
   }
 }
