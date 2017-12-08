4441cdc4819f4b36769f6551ef193ad9cc310917
Chris Lande <clande@marchex.com>
2017-12-08 13:57:48 -0800

    day 7 start

--- src/main/scala/aoc2017/Day7.scala
+++ src/main/scala/aoc2017/Day7.scala
@@ -1,12 +1,50 @@
 package aoc2017
 
+import scala.collection.mutable
+
+case class Node(name: String, var weight: Int, var children: List[Node], var parent: Node)
+
 class Day7 {
+  val nodes = mutable.HashMap[String, Node]()
   def getFileAndDoWork = {
-    val x = scala.io.Source.fromFile("input/DayX/input").mkString.toInt
+    val x = scala.io.Source.fromFile("input/Day7/input").mkString
     println(doWork(x))
   }
 
-  def doWork(target: Int): Int = {
-    0
+  def processEntry(entry: String): Node = {
+    val tokens = entry.replace(",", "").split(" ")
+
+    var children = tokens.length match {
+      case l if (l <= 2) => Nil
+      case _ => tokens.slice(3, tokens.length - 1).map(processEntry(_)).toList
+    }
+
+    val name = tokens(0)
+    val weight = if (tokens.length == 1) 0 else tokens(1).stripPrefix("(").stripSuffix(")").toInt
+    var newNode = Node(name, weight, children, null) //TODO: look up the nodes
+
+    val node = if(nodes.contains(newNode.name)) {
+      val preExistingNode = nodes(name)
+      if (newNode.children != Nil) preExistingNode.children = newNode.children
+      if (newNode.weight != 0) preExistingNode.weight = newNode.weight
+      preExistingNode
+    } else {
+      nodes += (newNode.name -> newNode)
+      newNode
+    }
+
+    if(node.children != Nil) node.children map(_.parent = node)
+
+    node
+  }
+
+  def doWork(input: String): String = {
+    val entries = input.split("\n")
+
+    entries.foreach(processEntry(_))
+
+    var currNode = nodes(nodes.keys.head)
+    while (null != currNode.parent) currNode = currNode.parent
+    currNode.name
   }
 }
--- src/test/scala/aoc2017/Day7Spec.scala
+++ src/test/scala/aoc2017/Day7Spec.scala
@@ -9,14 +9,27 @@ class Day7Spec extends FunSpecLike
   with Matchers
   with ScalaFutures {
   describe("doWork") {
-//    val x = new Day3
+    val x = new Day7
 
 //    it("should process the file") {
 //      x.getFileAndDoWork
 //    }
 
-//    it("should get 18") {
-//      x.doWork(10) shouldBe 15
-//    }
+    it("should get thnk") {
+      val input = """pbga (66)
+                    |xhth (57)
+                    |ebii (61)
+                    |havc (66)
+                    |ktlj (57)
+                    |fwft (72) -> ktlj, cntj, xhth
+                    |qoyq (66)
+                    |padx (45) -> pbga, havc, qoyq
+                    |tknk (41) -> ugml, padx, fwft
+                    |jptl (61)
+                    |ugml (68) -> gyxo, ebii, jptl
+                    |gyxo (61)
+                    |cntj (57)""".stripMargin
+      x.doWork(input) shouldBe "tnhk"
+    }
   }
 }
