46704c4d42923fbe8b505b587d604a960f21238d
Chris Lande <clande@marchex.com>
2017-12-21 08:24:08 -0800

    Day 19 part 1

--- src/main/scala/aoc2017/Day19.scala
+++ src/main/scala/aoc2017/Day19.scala
@@ -9,7 +9,10 @@ class Day19 {
   def processIntersection(x: Int, y: Int, direction: String, charMap: Array[Array[Char]]): (Int, Int, String) = {
     var (newX, newY) = (x, y)
     var newDirection: String = null
-    val directionsToCheck = Seq("down", "up", "left", "right").filter(_.equals(direction))
+    val directionsToCheck = direction match {
+      case c if(Seq("down","up").contains(c)) => Seq("left", "right")
+      case c if(Seq("left","right").contains(c)) => Seq("up", "down")
+    }
 
     directionsToCheck.foreach{ d =>
       val (x2, y2) = d match {
@@ -17,8 +20,9 @@ class Day19 {
         case "up" => (x, y - 1)
         case "left" => (x - 1, y)
         case "right" => (x + 1, y)
+        case d => throw new RuntimeException(s"Unexpected direction: $d")
       }
-      if(charMap(x2)(y2) != " ") {
+      if(!getChar(charMap,x2,y2).isWhitespace) {
         newDirection = d
         newX = x2
         newY = y2
@@ -28,25 +32,49 @@ class Day19 {
     (newX, newY, newDirection)
   }
 
+  def getChar(charMap: Array[Array[Char]], x: Int, y: Int): Char = {
+    if ((x > -1) && (y > -1) && charMap.length > y && charMap(y).length > x) charMap(y)(x) else ' '
+  }
+
   def findNextMapPosition(charMap: Array[Array[Char]], x: Int, y: Int, direction: String) = {
     var (newX, newY) = (x, y)
     var newDirection = direction
-    (charMap(x)(y), direction) match {
-      case ('|', "down") =>  newY += 1
-      case ('|', "up") => newY -= 1
-      case ('-', "left") => newX -= 1
-      case ('-', "right") => newX += 1
+    (getChar(charMap,x,y), direction) match {
+      case (c, _) if(c.isWhitespace) =>
+        newX = -1
+        newY = -1
+      case ('|', "down") =>
+        newY += 1
+      case ('-', "down") =>
+        newY += 1
+      case ('|', "up") =>
+        newY -= 1
+      case ('-', "up") =>
+        newY -= 1
+      case ('-', "left") =>
+        newX -= 1
+      case ('|', "left") =>
+        newX -= 1
+      case ('-', "right") =>
+        newX += 1
+      case ('|', "right") =>
+        newX += 1
       case ('+', _) => {
         val (x2, y2, d) = processIntersection(newX, newY, newDirection, charMap)
         newX = x2
         newY = y2
         newDirection = d
       }
-      case(c, "up") if(c.isLetter || c.isWhitespace) => newY -= 1
-      case(c, "down") if(c.isLetter || c.isWhitespace) => newY += 1
-      case(c, "right") if(c.isLetter || c.isWhitespace) => newX += 1
-      case(c, "left") if(c.isLetter || c.isWhitespace) => newX -= 1
-      case(c, d) => throw new RuntimeException(s"Bad current position x: $x, y: $y, char: $c, dir: $direction")
+      case (c, "up") if(c.isLetter) =>
+        newY -= 1
+      case (c, "down") if(c.isLetter) =>
+        newY += 1
+      case (c, "right") if(c.isLetter) =>
+        newX += 1
+      case (c, "left") if(c.isLetter) =>
+        newX -= 1
+      case (c, d) =>
+        throw new RuntimeException(s"Bad current position x: $x, y: $y, char: $c, dir: $direction")
     }
 
     (newX, newY, newDirection)
@@ -61,14 +89,14 @@ class Day19 {
     var pathString = ""
 
     //find entry point
-    (0 to mapLength - 1).foreach (i => if (!charMap(0)(i).isWhitespace) y = i)
+    (0 to mapLength - 1).foreach (i => if (!getChar(charMap,i,0).isWhitespace) x = i)
 
     while((x,y) != (-1,-1)){
       val (x2, y2, d) = findNextMapPosition(charMap, x, y, direction)
       x = x2
       y = y2
       direction = d
-      if (charMap(x)(y).isLetter) pathString += charMap(x)(y)
+      if (getChar(charMap,x,y).isLetter) pathString += getChar(charMap,x,y)
     }
 
     pathString
